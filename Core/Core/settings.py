1a794ee8b787553bdfddc803d84d5babd7b4e887
stigin starview <stiginstarview@hotmail.com>
2020-12-22 22:15:12 +0530

    initial commit - implimenting django allauth

--- Core/Core/settings.py
+++ Core/Core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'allauth',
+    # 'allauth',
     'allauth.account',
     'allauth.socialaccount',
     # 'allauth.socialaccount.providers.google'  # when the site is up and running try implimenting this.
