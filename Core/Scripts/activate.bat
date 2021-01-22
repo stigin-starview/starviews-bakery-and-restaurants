9b8e390dbfe2fba4ad123e44bf1f29011f7a508f
stigin starview <stiginstarview@hotmail.com>
2021-01-22 14:51:30 +0530

    intializing the project

--- Core/Scripts/activate.bat
+++ Core/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\I.T\projects\web development\starviewsBakeryAndRestaurants\Core
+set VIRTUAL_ENV=D:\I.T\projects\web development\starviewsBakeryAndRestaurants\CORE
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(Core) %PROMPT%
+set PROMPT=(CORE) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
